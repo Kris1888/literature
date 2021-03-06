2d0a305b67a145f55208d16d3565d7871df14f7a
Kris1888 <2356245162@qq.com>
2021-03-06 16:25:30 +0800

    修改

--- src/main/java/com/woniuxy/vo/UserVo.java
+++ src/main/java/com/woniuxy/vo/UserVo.java
@@ -3,9 +3,9 @@ package com.woniuxy.vo;
 import lombok.Data;
 
 @Data
-public class UserVo {
+public class UserVO {
     private String username;
     private String password;
     private String user_tel;
-    private  Boolean checked;
+    private Boolean checked;
 }
