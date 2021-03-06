42cbbfc3a9ee398c04818dbb5d4c3fc339f12607
Kris1888 <2356245162@qq.com>
2021-03-06 10:57:47 +0800

    角色查询与权限

--- src/main/java/com/woniuxy/mapper/PermissionMapper.java
+++ src/main/java/com/woniuxy/mapper/PermissionMapper.java
@@ -2,6 +2,9 @@ package com.woniuxy.mapper;
 
 import com.woniuxy.model.Permission;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,22 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2021-03-05
  */
 public interface PermissionMapper extends BaseMapper<Permission> {
+
+    @Select("SELECT p.id" +
+            "FROM t_role_permission rp" +
+            "JOIN t_permission p" +
+            "ON rp.pid=p.id" +
+            "WHERE rp.rid=#{rid}" +
+            "AND p.`level`=3")
+    List<Integer> getPermissionsIdByRid(Integer rid);
+
+    @Select("SELECT *" +
+            "FROM t_permission" +
+            "WHERE id=(" +
+            "SELECT p.pid" +
+            "FROM t_permission p" +
+            "WHERE p.id=#{pid}+" +
+            ")")
+    Permission getPermissionByPid(Integer pid);
 
 }
