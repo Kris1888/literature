f60d0b5d9d8001b565995a81bfc688055d617e5f
Kris1888 <2356245162@qq.com>
2021-03-08 16:38:23 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/main/java/com/woniuxy/controller/UserController.java
    #	src/main/java/com/woniuxy/filter/JwtFilter.java
    #	src/main/java/com/woniuxy/realm/Myrealm.java

--- src/main/java/com/woniuxy/component/CustomerRenlm.java
+++ src/main/java/com/woniuxy/component/CustomerRenlm.java
@@ -1,50 +1,50 @@
-//package com.woniuxy.component;
-//
-//import com.woniuxy.mapper.RoleMapper;
-//import com.woniuxy.mapper.UserMapper;
-//import com.woniuxy.model.Permission;
-//import com.woniuxy.model.User;
-//import org.apache.shiro.authc.AuthenticationException;
-//import org.apache.shiro.authc.AuthenticationInfo;
-//import org.apache.shiro.authc.AuthenticationToken;
-//import org.apache.shiro.authz.AuthorizationInfo;
-//import org.apache.shiro.authz.SimpleAuthorizationInfo;
-//import org.apache.shiro.realm.AuthorizingRealm;
-//import org.apache.shiro.subject.PrincipalCollection;
-//import org.apache.shiro.util.CollectionUtils;
-//
-//import javax.annotation.Resource;
-//import java.util.List;
-//
-//public class CustomerRenlm extends AuthorizingRealm {
-//    @Resource
-//    private UserMapper userMapper;
-//    @Resource
-//    private RoleMapper roleMapper;
-//    @Override
-//    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-//        //进行授权安排
-//        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-//        User user = (User) principalCollection.getPrimaryPrincipal();
-//        //查询当前用户拥有哪些角色
-//        List<String> roles = roleMapper.findRolesByUid(user.getUserId());
-//       if (!CollectionUtils.isEmpty(roles)){
-//           //如果查询到的角色集合不为空开始授予角色
-//
-//           roles.forEach(role -> simpleAuthorizationInfo.addRole(role));
-//       }
-//        //查询当前用户拥有哪些权限
+package com.woniuxy.component;
+
+import com.woniuxy.mapper.RoleMapper;
+import com.woniuxy.mapper.UserMapper;
+import com.woniuxy.model.Permission;
+import com.woniuxy.model.User;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class CustomerRenlm extends AuthorizingRealm {
+    @Resource
+    private UserMapper userMapper;
+    @Resource
+    private RoleMapper roleMapper;
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        //进行授权安排
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        User user = (User) principalCollection.getPrimaryPrincipal();
+        //查询当前用户拥有哪些角色
+        List<String> roles = roleMapper.findRolesByUid(user.getUserId());
+       if (!CollectionUtils.isEmpty(roles)){
+           //如果查询到的角色集合不为空开始授予角色
+
+           roles.forEach(role -> simpleAuthorizationInfo.addRole(role));
+       }
+        //查询当前用户拥有哪些权限
 //        List<Permission> permissions = userMapper.findMenusByUid(user.getUserId());
 //        if (!CollectionUtils.isEmpty(permissions)){
-//            //如果查询到的权限集合不为空开始授予角色
-//
+            //如果查询到的权限集合不为空开始授予角色
+
 //            permissions.forEach(permission -> simpleAuthorizationInfo.addStringPermission(permission.getPermissionName()));
 //        }
-//        return simpleAuthorizationInfo;
-//    }
-//
-//    @Override
-//    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-//        return null;
-//    }
-//}
+        return simpleAuthorizationInfo;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
--- src/main/java/com/woniuxy/controller/UserController.java
+++ src/main/java/com/woniuxy/controller/UserController.java
@@ -2,13 +2,23 @@ package com.woniuxy.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+
 import com.woniuxy.dto.Result;
 import com.woniuxy.dto.StatusCode;
+import com.woniuxy.mapper.UserMapper;
 import com.woniuxy.model.User;
+import com.woniuxy.realm.Myrealm;
 import com.woniuxy.service.UserService;
 import com.woniuxy.util.SaltUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.Md5Hash;
+
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.sound.midi.Soundbank;
 
 /**
  * <p>
@@ -29,14 +40,17 @@ import javax.annotation.Resource;
 @RequestMapping("/user")
 public class UserController {
     @Resource
+    private UserMapper userMapper;
+    @Resource
     private UserService userService;
-    //用户注册
-    @PostMapping("register")
-    public Result register(@RequestBody User user){
-        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
-        queryWrapper.eq("username",user.getUser_name());
-        User userDB=userService.getOne(queryWrapper);
-        if (ObjectUtils.isEmpty(userDB)){
+
+    @PostMapping("/register")
+    public Result register(@RequestBody User user) {
+        System.out.println(user);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_name", user.getUser_name());
+        User userDB = userService.getOne(queryWrapper);
+        if (ObjectUtils.isEmpty(userDB)) {
             String salt = SaltUtils.getSalt(8);
             Md5Hash md5Hash = new Md5Hash(user.getPassword(), salt, 2048);
             // 保存加密的密码
@@ -45,20 +59,42 @@ public class UserController {
             user.setSalt(salt);
             //        存到数据中
             userService.save(user);
-            return  new Result(true, StatusCode.OK,"注册成功");
-        }else {
-            return  new Result(true,StatusCode.OK,"已经被注册");
+            return new Result(true, StatusCode.OK, "注册成功");
+        } else {
+            return new Result(true, StatusCode.OK, "已经被注册");
         }
     }
+
+
 //    @PostMapping("login")
-//    public Result login(@RequestBody UserVo userVo) {
-////        System.out.println(userVo+"userV");
-////        System.out.println(userVo+"userV");
+//    public Result login(@RequestBody UserVO userVo) {
+//        System.out.println(userVo.getUser_name() + "user_name");//账号
+//        System.out.println(userVo.getPassword() + "password");//密码
+//        System.out.println(userVo.getUser_tel() + "user_tel");//电话
+//        System.out.println(userVo.getChecked() + "check");
+//
+//        UsernamePasswordToken token = new UsernamePasswordToken(userVo.getUser_name(),userVo.getPassword(),userVo.getChecked());
+//        if (!ObjectUtils.isEmpty(userVo)){
+//            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+//            queryWrapper.eq("user_name", userVo.getUser_name());
+//            User user = userMapper.selectOne(queryWrapper);
+//            System.out.println(user.getSalt());
+//            Md5Hash md5Hash = new Md5Hash(userVo.getPassword(),user.getSalt(),2048);
+//            String loginPassword = md5Hash.toHex();
+//            System.out.println(loginPassword);
+//            if (loginPassword.equals(user.getPassword())){
+//                return new Result(true, StatusCode.OK,"登录成功",user);
+//            }else {
+//                return new Result(false,StatusCode.ERROR,"错误");
+//            }
+//        }
+//
 ////        Subject subject = SecurityUtils.getSubject();
-////        UsernamePasswordToken token=new UsernamePasswordToken(userVo.getUsername(),userVo.getPassword());
+////        UsernamePasswordToken token = new UsernamePasswordToken(userVo.getUser_name(), userVo.getPassword(),userVo.getChecked());
 ////        subject.login(token);
 //
-//        return  new Result(true,StatusCode.OK,"登录成功",userVo);
-//}
+//        return new Result(true, StatusCode.OK, "登录成功", userVo);
+//    }
 }
+
 
