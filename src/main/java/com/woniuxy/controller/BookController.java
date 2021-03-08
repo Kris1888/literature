15adc6b94d4a9a1d50cb034e3ab573e79b23a636
Kris1888 <2356245162@qq.com>
2021-03-08 20:38:04 +0800

    Merge branch 'master' of github.com:/Kris1888/literature into krisY
    # Conflicts:
    #	src/main/java/com/woniuxy/controller/UserController.java
    #	src/main/java/com/woniuxy/model/User.java
    #	src/main/java/com/woniuxy/realm/JwtToken.java
    #	src/main/java/com/woniuxy/realm/Myrealm.java
    #	src/main/java/com/woniuxy/realm/ShiroConfig.java
    #	src/main/java/com/woniuxy/util/JWTUtil.java
    #	src/main/java/com/woniuxy/vo/UserVO.java

--- src/main/java/com/woniuxy/controller/BookController.java
+++ src/main/java/com/woniuxy/controller/BookController.java
@@ -2,11 +2,14 @@ package com.woniuxy.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.woniuxy.dto.Result;
 import com.woniuxy.dto.StatusCode;
 import com.woniuxy.mapper.BookMapper;
 import com.woniuxy.model.Book;
 import com.woniuxy.service.BookService;
+import com.woniuxy.vo.PageVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +42,15 @@ public class BookController {
         queryWrapper.eq("book_id",book_id);
         List<Book> books=bookService.list(queryWrapper);
         return new Result(true, StatusCode.OK,"查新书籍详情成功",books);
+    }
+    //分类书籍分页查询
+    @GetMapping("getAll")
+    public Result find2Page(PageVO pageVO){
+        System.out.println(pageVO);
+        //设置分页查询条件
+        Page<Book> bookPage=new Page<>(pageVO.getCurrent(),pageVO.getSize());
+        IPage<Book> page=bookService.page(bookPage,null);
+        return new Result(true,StatusCode.OK,"用户信息分页查询成功",page);
     }
 }
 
